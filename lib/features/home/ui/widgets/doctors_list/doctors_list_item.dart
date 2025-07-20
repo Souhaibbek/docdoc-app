0fe87d52badb67572634dfade92b3ba050888d59
Souhaibbek <Bekkaysouhaib1996@gmail.com>
2025-07-20 13:58:50 +0100

    add shimmer for the cached network image

--- lib/features/home/ui/widgets/doctors_list/doctors_list_item.dart
+++ lib/features/home/ui/widgets/doctors_list/doctors_list_item.dart
@@ -1,9 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:comeback/core/helpers/spacing.dart';
+import 'package:comeback/core/theming/colors.dart';
 import 'package:comeback/core/theming/styles.dart';
 import 'package:comeback/features/home/data/models/speciality_response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class DoctorsListItem extends StatelessWidget {
   final Doctors? doctorsModel;
@@ -20,13 +22,17 @@ class DoctorsListItem extends StatelessWidget {
                 'https://static.vecteezy.com/system/resources/thumbnails/026/375/249/small/ai-generative-portrait-of-confident-male-doctor-in-white-coat-and-stethoscope-standing-with-arms-crossed-and-looking-at-camera-photo.jpg',
 
             progressIndicatorBuilder: (context, url, downloadProgress) {
-              return Container(
-                width: 110.w,
-                height: 120.h,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(12.0),
-                  color: Colors.white,
+              return Shimmer.fromColors(
+                baseColor: ColorsManager.lightGrey ,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white,
+                  ),
                 ),
               );
             },
