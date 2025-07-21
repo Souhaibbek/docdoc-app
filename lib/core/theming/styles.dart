ee4307c11a1138968c9fe60456a0df9252b2f5dc
Souhaibbek <Bekkaysouhaib1996@gmail.com>
2025-07-21 12:10:49 +0100

    features/font-family-integrate
    -added google fonts package
    -apply the inter font family

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -2,123 +2,123 @@ import 'package:comeback/core/theming/colors.dart';
 import 'package:comeback/core/theming/font_weight.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Styles {
-  static TextStyle font24BlackBold = TextStyle(
+  static TextStyle font24BlackBold = GoogleFonts.inter(
     fontSize: 24.sp,
     fontWeight: FontWeightHelper.bold,
     color: Colors.black,
   );
-  static TextStyle font32BlueBold = TextStyle(
+  static TextStyle font32BlueBold = GoogleFonts.inter(
     fontSize: 32.sp,
     fontWeight: FontWeightHelper.bold,
     color: ColorsManager.mainBlue,
   );
-  static TextStyle font24BlueBold = TextStyle(
+  static TextStyle font24BlueBold = GoogleFonts.inter(
     fontSize: 24.sp,
     fontWeight: FontWeightHelper.bold,
     color: ColorsManager.mainBlue,
   );
-   static TextStyle font14GreyRegular = TextStyle(
+  static TextStyle font14GreyRegular = GoogleFonts.inter(
     fontSize: 14.sp,
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.greyColor,
   );
-  static TextStyle font14LightGreyMedium = TextStyle(
+  static TextStyle font14LightGreyMedium = GoogleFonts.inter(
     fontSize: 14.sp,
     fontWeight: FontWeightHelper.medium,
     color: ColorsManager.lightGrey,
   );
-  static TextStyle font14DarkBlueMedium = TextStyle(
+  static TextStyle font14DarkBlueMedium = GoogleFonts.inter(
     fontSize: 14.sp,
     fontWeight: FontWeightHelper.medium,
     color: ColorsManager.darkBlue,
   );
-   static TextStyle font14BlueRegular = TextStyle(
+  static TextStyle font14BlueRegular = GoogleFonts.inter(
     fontSize: 14.sp,
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.mainBlue,
   );
-  static TextStyle font16WhiteSemiBold =TextStyle(
+  static TextStyle font16WhiteSemiBold = GoogleFonts.inter(
     fontSize: 16.sp,
     fontWeight: FontWeightHelper.semiBold,
     color: Colors.white,
   );
-   static TextStyle font11GreyMedium = TextStyle(
+  static TextStyle font11GreyMedium = GoogleFonts.inter(
     fontSize: 11.sp,
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.greyColor,
   );
-  static TextStyle font11DarkBlueMedium = TextStyle(
+  static TextStyle font11DarkBlueMedium = GoogleFonts.inter(
     fontSize: 11.sp,
     fontWeight: FontWeightHelper.medium,
     color: ColorsManager.darkBlue,
   );
-   static TextStyle font11DarkBlueRegular = TextStyle(
+  static TextStyle font11DarkBlueRegular = GoogleFonts.inter(
     fontSize: 11.sp,
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.darkBlue,
   );
-   static TextStyle font11BlueSemiBold =TextStyle(
+  static TextStyle font11BlueSemiBold = GoogleFonts.inter(
     fontSize: 11.sp,
     fontWeight: FontWeightHelper.semiBold,
     color: ColorsManager.mainBlue,
   );
-    static TextStyle font14BlueSemiBold = TextStyle(
+  static TextStyle font14BlueSemiBold = GoogleFonts.inter(
     fontSize: 14.sp,
     fontWeight: FontWeightHelper.semiBold,
     color: ColorsManager.mainBlue,
   );
 
-  static TextStyle font15DarkBlueMedium = TextStyle(
+  static TextStyle font15DarkBlueMedium = GoogleFonts.inter(
     fontSize: 15.sp,
     fontWeight: FontWeightHelper.medium,
     color: ColorsManager.darkBlue,
   );
-     static TextStyle font14DarkBlueRegular = TextStyle(
+  static TextStyle font14DarkBlueRegular = GoogleFonts.inter(
     fontSize: 14.sp,
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.darkBlue,
-     );
-      static TextStyle font18DarkBlueBold = TextStyle(
+  );
+  static TextStyle font18DarkBlueBold = GoogleFonts.inter(
     fontSize: 18.sp,
     fontWeight: FontWeightHelper.bold,
     color: ColorsManager.darkBlue,
-     );
-     static TextStyle font11UltraGreyRegular = TextStyle(
+  );
+  static TextStyle font11UltraGreyRegular = GoogleFonts.inter(
     fontSize: 11.sp,
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.ultraGreyColor,
   );
-  static TextStyle font18WhiteMedium = TextStyle(
+  static TextStyle font18WhiteMedium = GoogleFonts.inter(
     fontSize: 18.sp,
     fontWeight: FontWeightHelper.medium,
     color: Colors.white,
   );
-    static TextStyle font12BlueRegular = TextStyle(
+  static TextStyle font12BlueRegular = GoogleFonts.inter(
     fontSize: 12.sp,
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.mainBlue,
   );
-  static TextStyle font12DarkBlueRegular = TextStyle(
+  static TextStyle font12DarkBlueRegular = GoogleFonts.inter(
     fontSize: 12.sp,
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.darkBlue,
   );
-    static TextStyle font14DarkBlueSemiBold = TextStyle(
+  static TextStyle font14DarkBlueSemiBold = GoogleFonts.inter(
     fontSize: 18.sp,
     fontWeight: FontWeightHelper.semiBold,
     color: ColorsManager.darkBlue,
   );
-  static TextStyle font16DarkBlueBold =TextStyle(
+  static TextStyle font16DarkBlueBold = GoogleFonts.inter(
     fontSize: 16.sp,
     fontWeight: FontWeightHelper.bold,
     color: ColorsManager.darkBlue,
   );
-   static TextStyle font12GreyMedium = TextStyle(
+  static TextStyle font12GreyMedium = GoogleFonts.inter(
     fontSize: 12.sp,
     fontWeight: FontWeightHelper.medium,
     color: ColorsManager.greyColor,
   );
- 
 }
